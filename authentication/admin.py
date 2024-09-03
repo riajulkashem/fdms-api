9582ec051e672fd3c51ee4047da0462eebfff411
Riajul Kashem <riajul.programmer@gmail.com>
2024-09-03 23:58:10 +0600

    customize admin for user model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
 
-# Register your models here.
+from authentication.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    )
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_filter = ("is_staff", "is_active", "date_joined")
+    # Add this line to exclude the password field from the list view
+    exclude = ("password",)
+    # Add this line to exclude the password field from the add/change view
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": ("groups", "user_permissions"),
+            },
+        ),
+        (
+            "Important dates",
+            {
+                "fields": ("last_login", "date_joined"),
+            },
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)
