0de5ad03900bfacd227474b51cdd3577ced1b7d3
Riajul Kashem <riajul.programmer@gmail.com>
2024-09-06 23:25:59 +0600

    update restaurant app models and update serializers

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,14 +5,36 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
 
+from authentication.models import Owner
+
 User = get_user_model()
 
 
-class TimeStampedModel(models.Model):
+class OperationLogModel(models.Model):
     """
     An abstract base class model that provides self-updating
-    ``created_at`` and ``updated_at`` fields.
+    common fields.
     """
+
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="creator_%(class)s_objects",
+        editable=False,
+        verbose_name="Created by",
+        help_text="The user who created this object.",
+    )
+
+    last_updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="updater_%(class)s_objects",
+        editable=False,
+        verbose_name="Updated by",
+        help_text="The user who last updated this object.",
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -21,15 +43,7 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
-class Company(TimeStampedModel):
-    """
-    A model representing a company.
-
-    Attributes:
-    -----------
-        name: The name of the company.
-        description: A description of the company.
-    """
+class Company(OperationLogModel):
 
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
@@ -38,32 +52,13 @@ class Company(TimeStampedModel):
         return self.name
 
 
-class Restaurant(TimeStampedModel):
-    """
-    A model representing a restaurant.
-
-    Attributes:
-    -----------
-        company: A foreign key to the Company model, representing the company that owns
-        the restaurant.
-        owner: A foreign key to the User model, representing the owner of the
-        restaurant.
-        name: A character field with a maximum length of 255 characters, representing
-        the name of the restaurant. It must be unique.
-        description: A text field, representing a description of the restaurant.
-        phone_number: A character field with a maximum length of 20 characters,
-        representing the phone number of the restaurant.
-        email: An email field, representing the email address of the restaurant.
-        website: A URL field, representing the website of the restaurant.
-        address: A character field with a maximum length of 255 characters,
-        representing the address of the restaurant.
-    """
+class Restaurant(OperationLogModel):
 
     company = models.ForeignKey(
         Company, on_delete=models.CASCADE, related_name="restaurants"
     )
     owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="restaurants"
+        Owner, on_delete=models.CASCADE, related_name="restaurants"
     )
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
@@ -76,18 +71,7 @@ class Restaurant(TimeStampedModel):
         return self.name
 
 
-class Menu(TimeStampedModel):
-    """
-    A model representing a menu for a restaurant.
-
-    Attributes:
-    -----------
-    restaurant: A foreign key to the Restaurant model, representing the restaurant
-    that the menu belongs to.
-    name: A character field with a maximum length of 255 characters, representing
-    the name of the menu.
-    description: A text field, representing a description of the menu.
-    """
+class Menu(OperationLogModel):
 
     restaurant = models.ForeignKey(
         Restaurant, on_delete=models.CASCADE, related_name="menus"
@@ -99,22 +83,11 @@ class Menu(TimeStampedModel):
         return f"{self.name} - {self.restaurant.name}"
 
 
-class Modifier(TimeStampedModel):
-    """
-    A model representing a modifier for a menu item.
+class Modifier(OperationLogModel):
 
-    Attributes:
-    -----------
-    name: A character field with a maximum length of 255 characters, representing
-    the name of the modifier.
-    price: A decimal field with a maximum of 10 digits and 2 decimal places,
-    representing the price of the modifier.
-    It must be greater than or equal to 0.
-    is_available: A boolean field, representing whether the modifier is currently
-    available. It defaults to True.
-    """
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name="modfiers"
+    )
     name = models.CharField(max_length=255)
     price = models.DecimalField(
         max_digits=10, decimal_places=2, validators=[MinValueValidator(0)]
@@ -125,18 +98,7 @@ class Modifier(TimeStampedModel):
         return self.name
 
 
-class Category(TimeStampedModel):
-    """
-    A model representing a category for menu items.
-
-    Attributes:
-    -----------
-    restaurant: A foreign key to the Restaurant model, representing the restaurant
-    that the category belongs to.
-    name: A character field with a maximum length of 255 characters, representing
-    the name of the category.
-    description: A text field, representing a description of the category.
-    """
+class Category(OperationLogModel):
 
     restaurant = models.ForeignKey(
         Restaurant, on_delete=models.CASCADE, related_name="categories"
@@ -148,26 +110,11 @@ class Category(TimeStampedModel):
         return self.name
 
 
-class Item(TimeStampedModel):
-    """
-    A model representing a menu item.
-    Attributes:
-    -----------
-    menu: A foreign key to the Menu model, representing the menu that
-    the item belongs to.
-    category: A foreign key to the Category model, representing the category
-    that the item belongs to.
-    name: A character field with a maximum length of 255 characters, representing
-    the name of the item.
-    description: A text field, representing a description of the item.
-    price: A decimal field with a maximum of 10 digits and 2 decimal places,
-    representing the price of the item. It must be greater than or equal to 0.
-    is_available: A boolean field, representing whether the item is currently available.
-    It defaults to True.
-    modifiers: A many-to-many relationship with the Modifier model, representing the
-    modifiers that can be added to the item. It is blank by default.
-    """
+class Item(OperationLogModel):
 
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name="items"
+    )
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="items")
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name="items"
@@ -184,33 +131,7 @@ class Item(TimeStampedModel):
         return self.name
 
 
-class Order(TimeStampedModel):
-    """
-    A model representing an order placed by a client for a restaurant.
-
-    Attributes:
-    -----------
-    order_id: A character field with a maximum length of 20 characters, representing
-    a unique identifier for the order.
-    It is automatically generated when the order is created and is not editable.
-    client: A foreign key to the User model, representing the client who placed
-    the order.
-    restaurant: A foreign key to the Restaurant model, representing the restaurant
-    that the order is for.
-    address: A character field with a maximum length of 500 characters, representing
-    the delivery address for the order.
-    items: A many-to-many relationship with the Item model, representing the items
-    that were ordered.
-    The relationship is defined through the OrderItem model.
-    total_amount: A decimal field with a maximum of 10 digits and 2 decimal places,
-    representing the total amount of the order.
-    It must be greater than or equal to 0.
-    payment_method: A character field with a maximum length of 4 characters,
-    representing the payment method used for the order.
-    It can be either "card" or "cash".
-    is_paid: A boolean field, representing whether the order has been paid for.
-    It defaults to False.
-    """
+class Order(OperationLogModel):
 
     PAYMENT_METHODS = [("card", "Card"), ("cash", "Cash")]
 
@@ -244,25 +165,7 @@ class Order(TimeStampedModel):
         return f"Order {self.order_id} - {self.client.get_full_name()}"
 
 
-class OrderItem(TimeStampedModel):
-    """
-    A model representing an item in an order.
-
-    Attributes:
-    -----------
-    order: A foreign key to the Order model, representing the order that the
-    item belongs to.
-    item: A foreign key to the Item model, representing the item that was ordered.
-    quantity: A positive integer field, representing the quantity of the item that
-    was ordered.
-    It must be greater than or equal to 1.
-    price: A decimal field with a maximum of 10 digits and 2 decimal places,
-    representing the price of the item.
-    It must be greater than or equal to 0.
-    modifiers: A many-to-many relationship with the Modifier model, representing
-    the modifiers that
-    were added to the item. It is blank by default.
-    """
+class OrderItem(OperationLogModel):
 
     order = models.ForeignKey(
         Order, on_delete=models.CASCADE, related_name="order_items"
--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -1,59 +1,79 @@
+from typing import Any
+
 from rest_framework import serializers
+
 from restaurants.models import (
+    Category,
     Company,
-    Restaurant,
+    Item,
     Menu,
     Modifier,
-    Category,
-    Item,
     Order,
     OrderItem,
+    Restaurant,
 )
 
 
-class CompanySerializer(serializers.ModelSerializer):
+class CustomModelSerializer(serializers.ModelSerializer):
+    def create(self, validated_data: Any) -> Any:
+        # Create the instance
+        instance = super().create(validated_data)
+
+        # Update the instance with the created_by field
+        instance.created_by = self.context["request"].user
+        instance.save()
+
+        return instance
+
+    def update(self, instance: Any, validated_data: Any) -> Any:
+        # Update the instance with the updated_by field
+        instance.last_updated_by = self.context["request"].user
+        return super().update(instance, validated_data)
+
+
+class CompanySerializer(CustomModelSerializer):
     class Meta:
         model = Company
-        fields = '__all__'
+        fields = "__all__"
 
 
-class RestaurantSerializer(serializers.ModelSerializer):
+class RestaurantSerializer(CustomModelSerializer):
     class Meta:
         model = Restaurant
-        fields = '__all__'
+        fields = "__all__"
 
 
-class MenuSerializer(serializers.ModelSerializer):
+class MenuSerializer(CustomModelSerializer):
     class Meta:
         model = Menu
-        fields = '__all__'
+        fields = "__all__"
 
 
-class ModifierSerializer(serializers.ModelSerializer):
+class ModifierSerializer(CustomModelSerializer):
     class Meta:
         model = Modifier
-        fields = '__all__'
+        fields = "__all__"
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(CustomModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = "__all__"
 
 
-class ItemSerializer(serializers.ModelSerializer):
+class ItemSerializer(CustomModelSerializer):
     class Meta:
         model = Item
-        fields = '__all__'
+        fields = "__all__"
 
 
-class OrderSerializer(serializers.ModelSerializer):
+class OrderSerializer(CustomModelSerializer):
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = "__all__"
 
 
-class OrderItemSerializer(serializers.ModelSerializer):
+class OrderItemSerializer(CustomModelSerializer):
     class Meta:
         model = OrderItem
-        fields = '__all__'
+        fields = "__all__"
