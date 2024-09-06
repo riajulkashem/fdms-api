b204494254bb57886299f9c43dc735bcbe599ad8
Riajul Kashem <riajul.programmer@gmail.com>
2024-09-06 23:35:06 +0600

    register all models to the admin

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from restaurants.models import (
+    Category,
+    Company,
+    Item,
+    Menu,
+    Modifier,
+    Order,
+    OrderItem,
+    Restaurant,
+)
+
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(Item)
+admin.site.register(Menu)
+admin.site.register(Modifier)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Restaurant)
